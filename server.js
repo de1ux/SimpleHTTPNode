8d4af90a9e3a3c28131d90295fa7fefbbeee868a
Nathan Evans <evans.nathan.j@gmail.com>
2014-12-31 22:18:56 -0700

    Some command line niceties and error handling

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
-
-var http    = require("http");
-var fs      = require("graceful-fs");
-var process = require("process");
-var path    = require("path");
+var http    = require('http');
+var fs      = require('fs');
 var url     = require("url");
-var port    = process.argv[2] || 9001;
-var absFolderPath = process.cwd()
-console.log("Serving directory " + absFolderPath + " on port " + port);
+var process = require('process');
+var argv    = require('yargs').argv;
+var port    = argv.port || argv.p || 9001;
 
 cors_headers = {
   "Access-Control-Allow-Origin": "*",
@@ -15,44 +12,53 @@ cors_headers = {
   "Access-Control-Allow-Headers": "Content-Type"
 }
 
-http.createServer(function (req, res) {
+// Hack sanitization job
+var folderToServe = process.argv[2] || '.';
+if (folderToServe.slice(0, 3) === '-p' || folderToServe.slice(0, 6) === '--port') {
+  folderToServe = '.';
+}
+if (folderToServe[folderToServe.length - 1] != '/') {
+  folderToServe += '/';
+}
+if (folderToServe[0] != '/') {
+  folderToServe = process.cwd() + '/' + folderToServe;
+}
+console.log("Serving HTTP on 0.0.0.0:"  + port + " for directory " + folderToServe);
+
+var main = function(req, res) {
   switch(req.url) {
-    case "/favicon.ico":
+    case "/favicon.ico" || undefined:
       res.writeHead(200);
       res.end();
       break;
     case "/":
     case "/.json":
-      fs.readdir(absFolderPath, function(err, files) {
-        try {
-          if(err) {
-            console.log(err);
-            res.writeHead(500);
-          } else {
-            console.log(files);
-            res.writeHead(200, cors_headers);
-            res.write(JSON.stringify(files))
-          }
-        } finally {
-          res.end();
-        }
-      });
+      console.log("Serving " + req.url);
+      var files;
+      try {
+        files = fs.readdirSync(folderToServe)
+            .map(function(v) {
+                return { name:v,
+                         time:fs.statSync(folderToServe + v).mtime.getTime()
+                       };
+             })
+             .sort(function(a, b) { return a.time - b.time; })
+             .map(function(v) { return v.name; });
+        res.writeHead(200, cors_headers);
+        res.write(JSON.stringify(files))
+      } catch (e) {
+        console.log(e);
+        res.writeHead(500);
+      } finally {
+        res.end();
+      }
       break;
     default:
       var uri = url.parse(req.url).pathname;
-      var url = absFolderPath + uri;
+      var path = folderToServe + uri;
       console.log("Serving " + uri);
 
-      fs.exists(url, function(exists) {
-        if (!exists) {
-          res.writeHead(404, {"Content-Type": "text/plain"});
-          res.write("404 Not Found\n");
-          res.end();
-          return;
-        }
-      });
-
-      fs.readFile(url, "binary", function(err, file) {
+      fs.readFile(path, "binary", function(err, file) {
         if(err) {
           res.writeHead(500, {"Content-Type": "text/plain"});
           res.write(err + "\n");
@@ -65,4 +71,12 @@ http.createServer(function (req, res) {
         res.end();
       });
   }
-}).listen(port);
+}
+
+var server = http.createServer(main)
+  .on('error', function (e) {
+    console.log(e);
+    console.log('Unable to start server');
+    console.log('Is port ' + port + ' available?');
+  }).listen(port);
+
